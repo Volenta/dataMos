f644fdfd6c35dc3f5c4dcb7d50d120ccaeae3178
Richard Nagelmaeker <richard@theinfotect.org>
2017-06-16 14:56:00 +0200

    defined spec for rdf-content in edn.
    Signed-off-by: Richard Nagelmaeker <richard@theinfotect.org>

--- src/datamos/spec/core.clj
+++ src/datamos/spec/core.clj
@@ -2,3 +2,44 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]
             [clojure.spec.test.alpha :as stest]))
+
+(s/def ::value string?)
+(s/def ::lang string?)
+(s/def ::type keyword?)
+
+(s/def ::literal-type
+  (s/keys :req-un [::value]
+          :opt-un [::type ::lang]))
+
+(s/def ::node-blank (s/map-of ::property ::object :conform-keys true))
+(s/def ::coll (s/coll-of ::object :kind vector? :min-count 1))
+
+(s/def ::subject-types (s/or :uri keyword?
+                             :blank-node ::node-blank
+                             :collection ::coll))
+
+(s/def ::object-types (s/or :uri keyword?
+                            :number number?
+                            :literal string?
+                            :typed-literal ::literal-type
+                            :boolean-literal boolean?
+                            :blank-node ::node-blank
+                            :collection ::coll))
+
+(s/def ::subject ::subject-types)
+(s/def ::property keyword?)                                 ;; Using property instead of predicate. Because Clojure already uses predicate in functions like s/explain
+(s/def ::statement (s/tuple ::subject ::property ::object))
+
+(s/def ::object ::object-types)
+(s/def ::graph (s/coll-of ::statement :kind set? :min-count 1))
+(s/def ::named-graphs (s/map-of keyword? ::graph :conform-keys true :min-count 1))
+
+(s/def ::prefix (s/map-of keyword? string? :conform-keys true :min-count 1))
+(s/def ::triples ::graph)
+(s/def ::quads ::named-graphs)
+
+(s/def ::rdf-content
+  (s/keys :req-un [::prefix]
+          :opt-un [::triples ::quads]))
+
+
