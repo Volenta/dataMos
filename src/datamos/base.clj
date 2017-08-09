45a4c6f34b8d9535447fcd093a7ebc874802c393
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-09 14:41:56 +0200

    v0.1.5.2

--- src/datamos/base.clj
+++ src/datamos/base.clj
@@ -15,18 +15,18 @@
   (keyword (str (namespace type-kw) "/" (name type-kw) "+dms-fn+" (java.util.UUID/randomUUID))))
 
 (defn set-component
-  "Returns component settings. With component-type, component-fn and component-uri as a submap of :datamos-cfg/module."
+  "Returns component settings. With component-type, module-fn and module-uri as a submap of :datamos-cfg/module."
   [settings]
   (let [{:keys [:datamos-cfg/module-fn
                 :datamos-cfg/module-type
                 :dms-def/provides
                 :dms-def/function
                 :datamos-cfg/local-register]
-         :or {component-type :datamos-fn/enrichment
-              component-fn :datamos-fn/function}} settings]
+         :or {module-type :datamos-fn/enrichment
+              module-fn :datamos-fn/function}} settings]
     (log/trace "@set-component" (log/get-env))
-    {(generate-qualified-uri component-fn) {:datamos-cfg/module-type component-type
-                                            :datamos-cfg/module-fn   component-fn
+    {(generate-qualified-uri module-fn) {:datamos-cfg/module-type module-type
+                                            :datamos-cfg/module-fn   module-fn
                                             :dms-def/provides           provides
                                             :datamos-cfg/local-register local-register
                                             :rdf/type                   :dms-def/module}}))
