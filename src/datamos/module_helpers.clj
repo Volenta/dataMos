b5a08bb30b9fe45f856f0df70ce9e6d54437b289
Richard Nagelmaeker <richard@theinfotect.org>
2017-08-11 12:10:12 +0200

    add fn get-prefix-matches

--- src/datamos/module_helpers.clj
+++ src/datamos/module_helpers.clj
@@ -1,9 +1,22 @@
-(ns datamos.module-helpers)
+(ns datamos.module-helpers
+  (:require [datamos
+             [communication :as dcom]
+             [messaging :as dm]]))
 
 (defn retrieve-prefixes
   [rdf-map]
-  (map keyword
+  (mapv keyword
        (set (keep namespace
                   (filter keyword?
                           (tree-seq coll? seq
                                     rdf-map))))))
+
+(defn get-prefix-matches
+  [speak-conn exchange-settings module-settings rdf-map]
+  (dcom/speak speak-conn
+              exchange-settings
+              module-settings
+              :datamos-fn/prefix
+              :dms-def/module
+              :datamos/match-prefix
+              {:dms-def/message {:dms-def/namespaces (retrieve-prefixes rdf-map)}}))
